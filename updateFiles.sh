3ea7fbd46dfad97cfebf60dde1df49b67d46cefa
Lorenzo Cardinali <lorenz.cardinali@studenti.unicam.it>
2022-08-12 22:55:53 +0200

    feature: added upgrade of sh files and Admin.txt

--- updateFiles.sh
+++ updateFiles.sh
@@ -7,14 +7,24 @@
 
 # path of the server folder
 SERVER_PATH="$(dirname "${BASH_SOURCE[0]}")"
-CONFIG_PATH=$SERVER_PATH"/serverfiles/Insurgency/Config/Server/"
+CONFIG_PATH="$SERVER_PATH/serverfiles/Insurgency/Config/Server/"
+TMP_PATH=$(mktemp -d)
+
+cd $SERVER_PATH || exit 1
 
 #echo server: $SERVER_PATH
 #echo config: $CONFIG_PATH
 #echo pwd: $(pwd)
 
-# files source
-ADMIN_FILE=https://raw.githubusercontent.com/LorenzoCardinali/SandstormServers/main/Admin.txt
+# clone of the repo in a temp directory
+git clone -q https://github.com/LorenzoCardinali/SandstormServers.git $TMP_PATH
 
-# update Admin.txt
-wget -qN $ADMIN_FILE -P $CONFIG_PATH
+# update scripts
+mv -f $TMP_PATH/scripts/* $SERVER_PATH
+chmod +x *.sh 
+
+# update Admins.txt
+mv -f $TMP_PATH/configs/Admins.txt $CONFIG_PATH
+
+# delete temp directory
+rm -rf $TMP_PATH
